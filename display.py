f83790f96d93d44d7ccca808a845fc125ead6e70
Sajed Nahian <snahian@stuy.edu>
2019-02-10 18:49:56 -0500

    Add files via upload

--- display.py
+++ display.py
@@ -23,6 +23,7 @@ def new_screen( width = XRES, height = YRES ):
 def plot( screen, color, x, y ):
     x += round(XRES/2)
     y += round(YRES/2)
+    print(x)
     newy = YRES - 1 - y
     if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES ):
         screen[newy][x] = color[:]
